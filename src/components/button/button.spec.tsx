5704f4c31a58936c55b287b887c866f399f74366
marcuszierke <marcuszierke@gmail.com>
2022-07-26 15:47:57 +0200

    test: first test to Button and App component added | mz

--- src/components/button/button.spec.tsx
+++ src/components/button/button.spec.tsx
@@ -1,9 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { AuthProvider } from '../../helpers';
+import { lightTheme } from '../../styles';
 import { Button } from './button';
 
-describe('Button', () => {
-	it('should render a button', () => {
-		render(<Button onClick={() => console.log('test')}>First Button</Button>);
-		expect(screen.getByText(/First Button/)).toBeInTheDocument();
+describe('Button Component', () => {
+	it('should render a default button', () => {
+		const { getByText } = render(
+			<AuthProvider>
+				<Button onClick={() => console.log('test')}>First Button</Button>
+			</AuthProvider>
+		);
+		expect(getByText(/First Button/)).toHaveStyle(
+			`background-color: ${lightTheme.button.default}; color: #FFF;}`
+		);
 	});
 });
