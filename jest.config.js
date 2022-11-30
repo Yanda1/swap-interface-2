73ac00183d42a2271b0fde432c4b4b91d1d89bfb
marcuszierke <marcuszierke@gmail.com>
2022-11-30 23:44:54 +0100

    fix: tsconfig.json fixed | mz

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	preset: 'ts-jest',
-	transform: {
-		'^.+\\.tsx?$': 'babel-jest'
-	}
+	testEnvironment: 'node',
+	testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)']
 };
