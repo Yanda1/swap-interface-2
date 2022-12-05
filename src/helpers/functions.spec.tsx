09aeaac016e64e77a25db5d621db6936c5b17c37
aliburzhi <aliburzhi033@gmail.com>
2022-12-05 11:38:05 +0200

    refactor:  add tests for new helper functions | ab

--- src/helpers/functions.spec.tsx
+++ src/helpers/functions.spec.tsx
@@ -1,13 +1,15 @@
-import { lightTheme, darkTheme } from '../styles';
+import { darkTheme, lightTheme } from '../styles';
 import {
-	isLightTheme,
 	beautifyNumbers,
-	isNetworkSelected,
-	isTokenSelected,
+	formatDate,
 	hexToRgbA,
 	isArrayType,
+	isLightTheme,
+	isNetworkSelected,
+	isSwapConfirmed,
+	isSwapFailed,
 	isSwapRejected,
-	formatDate
+	isTokenSelected
 } from '../helpers';
 
 describe('Helpers should return the correct values', () => {
@@ -52,6 +54,18 @@ describe('Helpers should return the correct values', () => {
 		expect(isSwapRejected('Exception', 'user confirmed transaction')).toBe(false);
 		expect(isSwapRejected('Success', 'user rejected transaction')).toBe(false);
 		expect(isSwapRejected('Success', 'user confirmed transaction')).toBe(false);
+	});
+
+	it('isSwapFailed() function should return the correct value', () => {
+		expect(isSwapFailed('Fail')).toBe(true);
+		expect(isSwapFailed('Exception')).toBe(false);
+		expect(isSwapFailed('Success')).toBe(false);
+	});
+
+	it('isSwapConfirmed() function should return the correct value', () => {
+		expect(isSwapConfirmed('Success')).toBe(true);
+		expect(isSwapConfirmed('Exception')).toBe(false);
+		expect(isSwapConfirmed('Fail')).toBe(false);
 	});
 
 	it('formatDate() function should return the correct value', () => {
